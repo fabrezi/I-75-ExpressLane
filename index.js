88239e4849abbc3d203a5a3690013791e2760882
fabrezi <fkschon@gmail.com>
2022-08-18 10:54:06 -0400

    server setup

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const app = express();
+const app = express(); //app is an instance of express
 
-app.get( '/', function (req, res) {
+//format of the command:
+//app.method(path, handler)
+//handler is the method that is executed when the route is matched
+
+app.get( '/',  (req, res) => {
     res.send('Hola Combrone');
 })
 
 app.listen(3000, () => {
     console.log('Server is up and running mate!!')
 });
+
